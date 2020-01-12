e4ff67538c76c40fead1daaaf04d078753ff7e3e
SadikhovEmin <eemin.sadykhov@gmail.com>
2020-01-12 14:50:44 +0300

    Initial Commit

---
+++ src/Model/Employee.java
@@ -0,0 +1,24 @@
+package Model;
+
+public abstract class Employee implements Expense {
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee " + id + " : " + name;
+    }
+}
